40739896319695dee756bd05131b9125db25913c
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-04-23 09:11:19 +0200

    Se agregó registro de usuarios

--- my_socialmedia_django/settings.py
+++ my_socialmedia_django/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     'profiles',
     'posts',
@@ -111,3 +113,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
