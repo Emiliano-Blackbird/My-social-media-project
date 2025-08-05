5da22c39a08c0b29155617a83e7fb4db7d3ff48f
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-08-05 21:53:20 +0200

    add styles for _footer, _header & nav

--- my_socialmedia_django/settings.py
+++ my_socialmedia_django/settings.py
@@ -110,6 +110,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]  # Directory for static files (added for Emi Blackbird's project)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
